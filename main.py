db06e7b569f33bffa6f3551d5a42c7c995302dd8
Lucas-Millart <lucas.millart@edu.esiee.fr>
2024-11-14 09:41:15 +0000

    exercice  palindrome 05

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 #### Fonction secondaire
-
-
+"""
+Ce module contient des fonctions pour vérifier si une chaîne de 
+caractère est un palindrome.
+"""
 def ispalindrome(p):
+    """
+    Retourne un booléen exprimant la vérité de "p est un palindrome".
 
+    Args: 
+        p: string à tester
+
+    Returns:
+        le booléen palindrome.
+    """
     # votre code ici
-    
+    p = p.lower()
+    accents = "àáâäãåçèéêëìíîïñòóôöõùúûüýÿ"
+    no_accents = "aaaaaaceeeeiiiinooooouuuuyy"
+    # Table permettant de supprimer les caractères inutiles
+    table = str.maketrans("","", " -,'?!.;:")
+    table2 = str.maketrans(accents,no_accents)
+    p = p.translate(table)
+    p = p.translate(table2)
+    p_inverse = p[::-1]
+    #test de la chaîne de caratère
+    if p == p_inverse:
+        return True
     return False
-
 #### Fonction principale
-
-
 def main():
-
+    """
+    Fonction principale pour tester les fonctions secondaires dont
+    ispalindrome
+    """
     # vos appels à la fonction secondaire ici
-
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie",
+    "Oh ! cela te perd, répéta l'écho"]:
         print(s, ispalindrome(s))
-
-
 if __name__ == "__main__":
     main()
